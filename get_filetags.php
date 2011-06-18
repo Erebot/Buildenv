ebfd450d1ab233862cd5a945bdcbb992f654aec3
François Poirotte <clicky@erebot.net>
2011-06-18 10:09:51 +0000

    Update the buildenv to support tagfiles.

---
+++ get_filetags.php
@@ -0,0 +1,66 @@
+#!/usr/bin/env php
+<?php
+/**
+ */
+
+function usage($script)
+{
+    echo "Usage: $script current_component [component1,component2,...]\n";
+    exit(1);
+}
+
+function main()
+{
+    $args = $_SERVER['argv'];
+    $script = array_shift($args);
+
+    if (!count($args))
+        usage($script);
+
+    $current = array_shift($args);
+
+    if (!count($args))
+        echo "\n";
+
+    $components = explode(',', array_shift($args));
+    $paths      = array();
+    $base       = dirname(dirname(dirname(dirname(__FILE__))));
+    if (!strncasecmp($current, 'Erebot_Module_', 14))
+        $base = dirname($base);
+
+    foreach ($components as $component) {
+        if ($current == '-')
+            $parts = array('', 'tmp', 'tagfiles');
+        else if ($component == 'Erebot' || $component == 'Plop')
+            $parts = array(
+                $base,
+                ($component == 'Erebot' ? 'core' : 'logging'),
+                'trunk',
+            );
+        else
+            $parts = array(
+                $base,
+                'modules',
+                substr($component, 14),
+                'trunk',
+            );
+
+        $parts[]        = $component . '.tagfile';
+        $path           = implode(DIRECTORY_SEPARATOR, $parts);
+        $paths[$path]   =
+            ($current == '-')
+            ? '../' . $component
+            : dirname($path) .
+                DIRECTORY_SEPARATOR . 'docs' .
+                DIRECTORY_SEPARATOR . 'html';
+    }
+
+    foreach ($paths as $tagfile => $relative)
+        echo "\"$tagfile=$relative\" ";
+
+    echo "\n";
+    exit(0);
+}
+
+main();
+
---
+++ get_locales.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+/**
+ * This program displays supported locales for i18n,
+ * as a comma-separated list.
+ */
+
+function usage($script)
+{
+    echo "Usage: $script [directory]\n";
+    exit(1);
+}
+
+function main()
+{
+    $args = $_SERVER['argv'];
+    $script = array_shift($args);
+
+    $dir = getcwd();
+    if (count($args))
+        $dir = array_shift($args);
+
+    $locales = array();
+
+    foreach (
+        new DirectoryIterator(
+            $dir.
+            DIRECTORY_SEPARATOR.'data'.
+            DIRECTORY_SEPARATOR.'i18n'
+        ) as $fileinfo) {
+        if (!$fileinfo->isDir() || $fileinfo->isDot())
+            continue;
+        $dirname = $fileinfo->getFilename();
+        if ($dirname[0] == ".")
+            continue;
+        $locales[] = $dirname;
+    }
+
+    echo implode(',', $locales)."\n";
+    exit(0);
+}
+
+main();
+
