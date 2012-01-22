eef1bdce12931f5b23f660bafcc429e69ea24139
François Poirotte <clicky@erebot.net>
2012-01-22 17:52:42 +0100

    Fix typo.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -26,7 +26,7 @@ execfile('patches.py')
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-+extensions = [
+extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.pngmath',
