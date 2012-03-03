3f7134bcb0be4aa90c823161478d9b8816097fd1
François Poirotte <clicky@erebot.net>
2012-03-03 22:46:53 +0100

    Fix OpenSearch URL.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -190,7 +190,7 @@ html_show_sourcelink = False
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = 'http://fpoirotte.github.com/Erebot/'
+#html_use_opensearch = 'http://erebot.github.com/Erebot/'
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = None
