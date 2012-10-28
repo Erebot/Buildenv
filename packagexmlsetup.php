eee5178d599417dffd1ea5fe4aec0b5747dc3d8e
François Poirotte <clicky@erebot.net>
2012-10-28 20:41:29 +0100

    First try at supporting composer.

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -5,9 +5,8 @@
  * More information: http://pear.php.net/manual/en/pyrus.commands.make.php#pyrus.commands.make.packagexmlsetup
  *
  * The defaults presented here take dependency information
- * and licensing information from data/package.php expressed
- * using Composer's terminology and convert them into PEAR
- * metadata.
+ * and licensing information from data/composer.json and
+ * convert them into PEAR metadata.
  */
 
 require(
@@ -15,19 +14,28 @@ require(
     DIRECTORY_SEPARATOR . 'dependency.php'
 );
 
-$packageName = $package->name;
-$packageVersion = $package->getReleaseVersion();
-$metadata = array($package->getChannel(). '/' . $packageName => array());
-require(
+$licenses = include(
     dirname(dirname(__FILE__)) .
-    DIRECTORY_SEPARATOR . 'data' .
-    DIRECTORY_SEPARATOR . 'package.php'
+    DIRECTORY_SEPARATOR . 'buildenv' .
+    DIRECTORY_SEPARATOR . 'licenses.php'
+);
+$metadata = json_decode(
+    file_get_contents(
+        dirname(dirname(__FILE__)) .
+        DIRECTORY_SEPARATOR . 'data' .
+        DIRECTORY_SEPARATOR . 'composer.json'
+    ),
+    TRUE
 );
 
+if ($metadata === NULL) {
+    throw new Exception('Could not parse composer.json');
+}
+
 $pearTypes = array(
-    'required'  => 'requires',
-    'optional'  => 'suggests',
-    'conflicts' => 'conflicts',
+    'required'  => 'require',
+    'optional'  => 'suggest',
+    'conflicts' => 'conflict',
 );
 
 function normalizePearVersion($package, $version)
@@ -45,72 +53,61 @@ function normalizePearVersion($package, $version)
     return $version;
 }
 
-$parser     = new Erebot_Package_Dependency();
-$fullName   = $package->getChannel(). '/' . $packageName;
-foreach (array($package, $compatible) as $obj) {
-    // requirements, suggestions and conflicts.
-    foreach ($pearTypes as $pearType => $composerType) {
-        if (!isset($metadata[$fullName][$composerType]))
+$parser = new Erebot_Package_Dependency();
+
+// Requirements, suggestions and conflicts.
+foreach ($pearTypes as $pearType => $composerType) {
+    if (!isset($metadata[$composerType]))
+        continue;
+
+    foreach ($metadata[$composerType] as $dependency => $constraints) {
+        // Skip virtual packages.
+        if (!strncasecmp('virt-', $dependency, 5))
             continue;
 
-        foreach ($metadata[$fullName][$composerType] as $dependency => $constraints) {
-            if (is_int($dependency)) {
-                $dependency     = $constraints;
-                $constraints    = '*';
-            }
+        // Determine minimal/maximal/excluded versions.
+        list($min, $max, $excluded) = $parser->getBounds($constraints);
 
-            // Skip virtual packages.
-            if (!strncasecmp('virt-', $dependency, 5))
-                continue;
-            // Determine minimal/maximal/excluded versions.
-            list($min, $max, $excluded) = $parser->getBounds($constraints);
-
-            // Grab a reference to the object the constraints apply to.
-            if ($dependency == 'php')
-                $objDep = $obj->dependencies[$pearType]->php;
-            else if ($dependency == 'pear2.php.net/pyrus') {
-                // This constraint only applies to Pyrus (PEAR2).
-                if ($obj === $compatible)
-                    continue;
-                $objDep = $obj->dependencies[$pearType]->pearinstaller;
-            }
-            else if (substr($dependency, 0, 13) == 'pecl.php.net/') {
-                $objDep = $obj->dependencies[$pearType]
+        // Grab a reference to the object the constraints apply to.
+        if ($dependency == 'php') {
+            $objDep = $package->dependencies[$pearType]->php;
+        }
+        else if (substr($dependency, 0, 13) == 'pecl.php.net/') {
+            $objDep = $package->dependencies[$pearType]
                               ->extension[substr($dependency, 13)];
-            }
-            else
-                $objDep = $obj->dependencies[$pearType]->package[$dependency];
-
-            // Apply the constraints.
-            if ($min !== NULL)
-                $objDep->min(normalizePearVersion($dependency, $min));
-            if ($max !== NULL)
-                $objDep->max(normalizePearVersion($dependency, $max));
-            foreach ($excluded as $version)
-                $objDep->exclude(normalizePearVersion($dependency, $version));
-            if ($pearType == 'conflicts')
-                $objDep->conflicts();
-            $objDep->save();
         }
-    }
-
-    // license.
-    if (isset($metadata[$fullName]['license'])) {
-        $licenses = (array) $metadata[$fullName]['license'];
-        if (count($licenses) != 1)
-            throw new Exception('Multiple licenses are not implemented yet');
-        foreach ($licenses as $name => $uri) {
-            if (is_int($name)) {
-                $name   = $uri;
-                $uri    = NULL;
-            }
-            $obj->license['name'] = $name;
-            if ($uri !== NULL)
-                $obj->license['uri'] = $uri;
+        else {
+            $objDep = $package->dependencies[$pearType]->package[$dependency];
         }
-    }
 
-    // Fix API stability.
-    $obj->stability['api'] = 'stable';
+        // Apply the constraints.
+        if ($min !== NULL)
+            $objDep->min(normalizePearVersion($dependency, $min));
+        if ($max !== NULL)
+            $objDep->max(normalizePearVersion($dependency, $max));
+        foreach ($excluded as $version)
+            $objDep->exclude(normalizePearVersion($dependency, $version));
+        if ($pearType == 'conflicts')
+            $objDep->conflicts();
+        $objDep->save();
+    }
 }
 
+// PEAR installer.
+$package->dependencies['required']->pearinstaller->min('1.9.2')->save();
+
+// license.
+if (isset($metadata['license'])) {
+    $pkgLicenses = (array) $metadata['license'];
+    if (count($pkgLicenses) != 1)
+        throw new Exception('Multiple licenses are not implemented yet');
+    foreach ($pkgLicenses as $name) {
+        $package->license['name'] = $name;
+        if (isset($licenses[$name])) {
+            $package->license['uri'] = sprintf(
+                'http://www.spdx.org/licenses/%s#licenseText',
+                rawurlencode($name)
+            );
+        }
+    }
+}
