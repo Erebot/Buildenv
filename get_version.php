8ae8623ff0220d139b5175f5c4c031a217d65a71
François Poirotte <clicky@erebot.net>
2013-07-13 19:33:33 +0200

    Fix buildenv.

--- get_version.php
+++ get_version.php
@@ -1,62 +1,33 @@
 #!/usr/bin/env php
 <?php
 /**
- * This program displays information about versioning:
- * - for a RELEASE
- * - for the API
+ * This program displays the current version.
  */
-
-function usage($script)
-{
-    echo "Usage: $script <options> <API|RELEASE> [directory]\n";
-    echo "where options can be:\n";
-    echo "-f\tDisplay latest file\n";
-    echo "or\n";
-    echo "-v\tDisplay matching version\n";
-    exit(1);
-}
 
 function main()
 {
     $args = $_SERVER['argv'];
     $script = array_shift($args);
-    if (!count($args))
-        usage($script);
-
-    $options = ltrim(array_shift($args), '-');
-    if (!in_array($options, array('f', 'v')))
-        usage($script);
-
-    if (!count($args))
-        usage($script);
-
-    $type = strtoupper(array_shift($args));
-    if (!in_array($type, array('API', 'RELEASE')))
-        usage($script);
 
     $dir = getcwd();
     if (count($args))
         $dir = array_shift($args);
 
-    $files = array();
-    foreach (new RegexIterator(
-            new DirectoryIterator($dir),
-           '/^'.$type.'\-(.+)$/',
-           RegexIterator::GET_MATCH
-        ) as $file) {
-        $files[$file[1]] = $file;
+    putenv('GIT_DIR=' . $dir . DIRECTORY_SEPARATOR . '.git');
+    $NUL = strncasecmp(PHP_OS, 'Win', 3) ? '/dev/null' : 'NUL';
+
+    exec('git describe --tags 2>' . $NUL, $output, $exitcode);
+    if ($exitcode != 0) {
+        unset($output);
+        exec('git describe --all 2>' . $NUL, $output, $exitcode);
+        $version = 'dev-' . substr(strstr(trim($output[0]), '/'), 1);
+    }
+    else {
+        $version = trim($output[0]);
     }
 
-    if (!count($files))
-        exit(2);
-
-    uksort($files, 'version_compare');
-    list($file, $version) = array_pop($files);
-    if ($options == "f")
-        echo "$file\n";
-    else
-        echo "$version\n";
-    exit(0);
+    echo $version . PHP_EOL;
+    exit($exitcode);
 }
 
 main();
